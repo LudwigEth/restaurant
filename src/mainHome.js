98fcd0a8f0b9b93ca467fd34eb27c950d96b7dbc
LudwigEth <ludwig.eth@gmail.com>
2023-10-23 22:24:21 +0200

    Feat: add homeCard container div

--- src/mainHome.js
+++ src/mainHome.js
@@ -1,12 +1,14 @@
 export default function() {
     const main = document.getElementById('main');
 
+    const newCardDiv = document.createElement('div');
     const newDiv = document.createElement('div');
     const newH2 = document.createElement('h2');
     const newP1 = document.createElement('p');
     const newP2 = document.createElement('p');
     const newQuote = document.createElement('blockquote');
 
+    newCardDiv.classList.add('homeCard');
     newDiv.classList.add('home');
     newQuote.classList.add('home');
 
@@ -18,6 +20,7 @@ export default function() {
     newDiv.appendChild(newH2);
     newDiv.appendChild(newP1);
     newDiv.appendChild(newP2);
-    main.appendChild(newDiv);
-    main.appendChild(newQuote);
+    newCardDiv.appendChild(newDiv);
+    newCardDiv.appendChild(newQuote);
+    main.appendChild(newCardDiv);
 }
