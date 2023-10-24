f2eca90d79daa82344a7f55200937763558a50d7
LudwigEth <ludwig.eth@gmail.com>
2023-10-24 19:27:13 +0200

    Feat: new webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,6 +10,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html'
     }),
   ],
   module: {
