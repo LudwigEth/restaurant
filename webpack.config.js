95332d7bdc2c8afbfb2e8c849edf48a3e13e23ae
LudwigEth <ludwig.eth@gmail.com>
2023-10-24 19:31:09 +0200

    Feat: mv index.html to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,12 +6,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-  ],
   module: {
     rules: [
       {
